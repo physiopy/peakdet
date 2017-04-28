1f7999da1776c4c426eebd0d8503c4211f164a80
Ross Markello <rdm222@cornell.edu>
2017-04-28 17:06:03 -0400

    End of day commit - RVT
    Starting work to implement RVT calculations into peakdet and finishing up the
    work week so need to commit for weekend shenanigans.

--- peakdet/modalities.py
+++ peakdet/modalities.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
 
 from __future__ import absolute_import
-import numpy as np
 from peakdet import physio
-from peakdet import utils
+from scipy.interpolate import interp1d
 
 
 class ECG(physio.PeakFinder):
@@ -31,6 +30,23 @@ class RESP(physio.PeakFinder):
     Class for respiratory data analysis
     """
 
-    def __init__(self, data, fs):
+    def __init__(self, data, fs, TR=None):
         super(RESP,self).__init__(data, fs)
-        self.lowpass([0.5])
+        self.bandpass([0.01,0.5])
+        self._TR = TR
+
+    @property
+    def TR(self):
+        return self._TR
+
+    @TR.setter
+    def TR(self, value):
+        self._TR = float(value)
+
+    @property
+    def RVT(self, TR=None):
+        if TR is not None: self.TR = TR
+        if self.TR is None: raise ValueError('TR needs to be set!')
+
+        peaks   = self.rawdata[self.peakinds]
+        heights = self.rawdata[self.troughinds]
