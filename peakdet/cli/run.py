c6ec105116d36aa16f6d663bf5fa997db9a39c88
Ross Markello <rossmarkello@gmail.com>
2018-03-18 17:09:36 -0400

    [ENH] New workflow

--- peakdet/cli/run.py
+++ peakdet/cli/run.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-import argparse
+import glob
 import os
 import warnings
 from gooey import Gooey, GooeyParser
@@ -10,87 +10,158 @@ modalities = dict(ECG=peakdet.ECG,
                   PPG=peakdet.PPG,
                   RESP=peakdet.RESP)
 
+attr_conv = {'Average NN intervals': 'avgnn',
+             'Root mean square of successive differences': 'rmssd',
+             'Standard deviation of NN intervals': 'sdnn',
+             'Standard deviation of successive differences': 'sdsd',
+             'Number of successive differences >50 ms': 'nn50',
+             'Percent of successive differences >50 ms': 'pnn50',
+             'Number of successive differences >20 ms': 'nn20',
+             'Percent of successive differences >20 ms': 'pnn20',
+             'High frequency HRV hfHRV': 'hf',
+             'Log of high frequency HRV, log(hfHRV)': 'hf_log',
+             'Low frequency HRV, lfHRV': 'lf',
+             'Log of low frequency HRV, log(lfHRV)': 'lf_log',
+             'Very low frequency HRV, vlfHRV': 'vlf',
+             'Log of very low frequency HRV, log(vlfHRV)': 'vlf_log',
+             'Ratio of lfHRV : hfHRV': 'lftohf',
+             'Peak frequency of hfHRV': 'hf_peak',
+             'Peak frequency of lfHRV': 'lf_peak'}
 
-@Gooey
+
+@Gooey(program_name='Physio pipeline',
+       program_description='Physiological processing pipeline',
+       default_size=(800, 1000))
 def get_parser():
-    parser = GooeyParser(description='Physiological processing '
-                                              'pipeline')
-    parser.add_argument('files', nargs='+', help='Files to be processed',
-                        widget='FileChooser')
-    parser.add_argument('-o', '--output', metavar='file',
-                        default='peakdet.csv', help='Output filename. '
-                        'Default: peakdet.csv')
-    parser.add_argument('--modality', default='ECG',
-                        choices=['ECG', 'PPG', 'RESP'],
-                        help='Modality of input data. Default: ECG')
-    parser.add_argument('--fs', metavar='sampling rate', default=1000,
-                        type=float, help='Sampling rate of input data. '
-                        'Default: 1000')
-    parser.add_argument('-m', '--measurements', nargs='+', widget='Listbox',
-                        choices=['avgnn', 'rmssd', 'sdnn', 'hf', 'hf_log',
-                                 'lf', 'lf_log'],
-                        help='Desired physiological measurements')
-    parser.add_argument('--col', metavar='column', default=0, type=int,
-                        help='Which column of data to read from input files. '
-                        'Default: 0')
-    parser.add_argument('-d', '--header', dest='header', action='store_true',
-                        help='Whether input files have header. Default: False')
-    parser.add_argument('-n', '--noedit', action='store_true',
-                        help='Turn off interactive editing.')
-    parser.add_argument('-t', '--thresh', dest='thresh', default=None,
-                        type=float, help=argparse.SUPPRESS)
+    """ Parser for GUI and command-line arguments """
+    parser = GooeyParser()
+    parser.add_argument('file_template', metavar='Filename template',
+                        widget='FileChooser',
+                        help='Select a representative file and replace '
+                        'all subject-specific information with a "?" symbol. '
+                        '\nE.g., subject_001_data.txt would become '
+                        'subject_???_data.txt')
+    inp_group = parser.add_argument_group('Inputs', 'Options to specify '
+                                          'format of input files')
+    inp_group.add_argument('--modality', metavar='Modality', default='ECG',
+                           choices=['ECG', 'PPG', 'RESP'],
+                           help='Modality of input data. Default: ECG')
+    inp_group.add_argument('--fs', metavar='Sampling rate', default=1000,
+                           type=float, help='Sampling rate of input data. '
+                           'Default: 1000')
+    inp_group.add_argument('--col', metavar='Column', default=0, type=int,
+                           help='Which column of data to read from input '
+                           'files. Default: 0')
+    inp_group.add_argument('-d', '--header', metavar='Header',
+                           action='store_true', help='Whether input files '
+                           'have header')
+    out_group = parser.add_argument_group('Outputs', 'Options to specify '
+                                          'format of output files')
+    out_group.add_argument('-o', '--output', metavar='Filename',
+                           default='peakdet.csv', help='Output filename. '
+                           'Default: peakdet.csv')
+    out_group.add_argument('-m', '--measurements', metavar='Measurements',
+                           nargs='+', widget='Listbox',
+                           choices=list(attr_conv.keys()),
+                           help='Desired physiological measurements.\nChoose '
+                           'multiple with ctrl+select')
+    edit_group = parser.add_argument_group('Workflow argument (optional!)',
+                                           'Options to specify modifications '
+                                           'to workflow')
+    edit_group.add_argument('-n', '--noedit', metavar='Editing',
+                            action='store_true',
+                            help='Turn off interactive editing.')
+    edit_group.add_argument('-t', '--thresh', dest='thresh',
+                            metavar='Threshold', default=None,
+                            type=float, help='Threshold for peak '
+                            'detection algorithm')
 
     return parser
 
 
-def main():
-    options = get_parser().parse_args()
-    peakfinder = modalities[options.modality]
+def workflow(*, file_template, modality, fs, col, header, output, measurements,
+             noedit, thresh):
+    """
+    Basic workflow for physiological data
+
+    Parameters
+    ----------
+    file_template : str
+        Template filename for data inputs
+    modality : str
+        One of [ECG, PPG, RESP]
+    fs : float
+        Sampling rate of input data
+    col : int
+        Column of input file where data is stored
+    header : bool
+        Whether input files have header
+    output : str
+        Path to output file
+    measurements : list-of-str
+        Desired output datapoints
+    noedit : bool
+        Whether to enable interactive editing
+    thresh : float
+        Threshold for peak detection
+    """
+
+    # output file
+    print('OUTPUT FILE:\t\t{}\n'.format(output))
+    # grab files from file template
+    print('FILE TEMPLATE:\t{}\n'.format(file_template))
+    files = glob.glob(file_template, recursive=True)
+
+    # convert measurements to peakdet.HRV attribute friendly names
+    print('REQUESTED MEASUREMENTS: {}\n'.format(', '.join(measurements)))
+    measurements = [attr_conv[attr] for attr in measurements]
+
+    # get appropriate modality
+    peakfinder = modalities[modality]
 
     # check if output file exists -- if so, ensure headers will match
-    header = 'filename,' + ','.join(options.measurements)
-    if os.path.exists(options.output):
-        with open(options.output, 'r') as src:
+    head = 'filename,' + ','.join(measurements)
+    if os.path.exists(output):
+        with open(output, 'r') as src:
             eheader = src.readlines()[0]
         # if existing output file does not have same measurements are those
         # requested on command line, warn and use existing measurements so
         # as not to totally fork up existing file
-        if eheader != header:
+        if eheader != head:
             warnings.warn('Desired output file exists and requested '
                           'measurements do not match with existing outputs '
                           'Using existing measurements, instead.')
             measurements = eheader.split(',')[1:]
-        header = ''
+        head = ''
     # if output file doesn't exist, nbd
     else:
-        header += '\n'
-        measurements = options.measurements[:]
+        head += '\n'
 
-    with open(options.output, 'a+') as dest:
-        dest.write(header)
+    with open(output, 'a+') as dest:
+        dest.write(head)
         # iterate through all files and do peak detection with manual editing
-        for fname in options.files:
+        for fname in files:
+            print('Currently processing {}'.format(fname))
             # load data into class instance
-            pf = peakfinder(fname, fs=options.fs,
-                            col=options.col, header=options.header)
-
+            pf = peakfinder(fname, fs=fs, col=col, header=header)
             # perform peak detection
-            if options.thresh is not None:
-                pf.get_peaks(thresh=options.thresh)
+            if thresh is not None:
+                pf.get_peaks(thresh=thresh)
             else:
                 pf.get_peaks()
-
             # edit peaks, if desired (HIGHLY RECOMMENDED)
-            if not options.noedit:
+            if not noedit:
                 pf.edit_peaks()
-
             # keep requested outputs
             hrv = peakdet.HRV(pf)
-            outputs = ['{:.5f}'.format(getattr(hrv, attr, None)) for attr in
+            outputs = ['{:.5f}'.format(getattr(hrv, attr, '')) for attr in
                        measurements]
-
             # save as we go so that interruptions don't screw everything up
             dest.write(','.join([fname] + outputs) + '\n')
+
+
+def main():
+    workflow(**vars(get_parser().parse_args()))
 
 
 if __name__ == '__main__':
