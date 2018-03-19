9346c309d3f33791e186e079840fd3b2ac5a9432
Ross Markello <rossmarkello@gmail.com>
2018-03-18 21:50:40 -0400

    [ENH] Setup.py fix

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ def main():
 
     if sys.version_info < (3, 6):
         raise SystemError("You need Python version 3.6 or above to use " +
-                          "pyls.")
+                          "peakdet.")
 
     # from nipype setup.py file
     ldict = locals()
     curr_path = os.path.dirname(__file__)
-    ver_file = os.path.join(curr_path, 'pyls', 'info.py')
+    ver_file = os.path.join(curr_path, 'peakdet', 'info.py')
     with open(ver_file) as infofile:
         exec(infofile.read(), globals(), ldict)
 
