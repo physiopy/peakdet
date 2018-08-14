340f4af2261652f3dd71cf40c30235dd8cddb6ee
Ross Markello <rossmarkello@gmail.com>
2018-08-13 23:03:27 -0400

    [STY] Add __version__ import in __init__.py

--- peakdet/__init__.py
+++ peakdet/__init__.py
@@ -1,9 +1,10 @@
 __all__ = [
     'edit_physio', 'filter_physio', 'interpolate_physio', 'peakfind_physio',
     'plot_physio', 'load_physio', 'save_physio', 'load_history',
-    'save_history', 'load_rtpeaks'
+    'save_history', 'load_rtpeaks', '__version__'
 ]
 
+from peakdet.info import (__version__)
 from peakdet.operations import (edit_physio, filter_physio, interpolate_physio,
                                 peakfind_physio, plot_physio)
 from peakdet.io import (load_physio, save_physio, load_history, save_history)
