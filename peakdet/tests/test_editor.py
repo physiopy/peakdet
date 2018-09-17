371ca2003f13b26158898fb24d953e1d8d1d17f0
Ross Markello <rossmarkello@gmail.com>
2018-09-16 20:54:46 -0400

    [TEST] Testing editor._PhysioEditor

--- peakdet/tests/test_editor.py
+++ peakdet/tests/test_editor.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+from sklearn.utils import Bunch
 from peakdet import editor
 from peakdet.tests.utils import get_peak_data
 
 PEAKS = get_peak_data()
 
 
-@pytest.mark.xfail
 def test_PhysioEditor():
-    assert False
+    edits = editor._PhysioEditor(get_peak_data())
+    # test scroll functionality
+    edits.on_wheel(Bunch(step=10))
+    # test reject / delete functionality
+    for m in range(2):
+        edits.on_reject(0, 10)
+        edits.on_delete(10, 20)
+    # undo delete + reject
+    for m in range(2):
+        edits.undo()
+    # test key undo (and undo when history doesn't exist)
+    edits.on_key(Bunch(key='ctrl+z'))
+    # redo so that there is history on quit
+    edits.on_reject(0, 10)
+    edits.on_delete(10, 20)
+    # quit editor and clean up edits
+    edits.on_key(Bunch(key='ctrl+q'))
+
+    with pytest.raises(TypeError):
+        editor._PhysioEditor([0, 1, 2])
 
 
 def test_delete_peaks():
