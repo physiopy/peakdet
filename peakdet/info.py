53b490c392c0f09d6bde1508220f1f5376d6955b
Stefano Moia <s.moia@bcbl.eu>
2021-01-22 16:31:47 +0100

    Reorganise setup file and contained information

--- peakdet/info.py
+++ peakdet/info.py
@@ -1,49 +1,5 @@
-NAME = 'peakdet'
-MAINTAINER = 'Ross Markello'
-EMAIL = 'rossmarkello@gmail.com'
-LICENSE = 'Apache-2.0'
-DESCRIPTION = """\
-A toolbox for reproducible physiological data analysis\
-"""
-LONG_DESCRIPTION = 'README.rst'
-LONG_DESCRIPTION_CONTENT_TYPE = 'text/x-rst'
-URL = 'https://github.com/physiopy/{name}'.format(name=NAME)
-DOWNLOAD_URL = ('http://github.com/physiopy/{name}/archive/'
-                .format(name=NAME))
-
-INSTALL_REQUIRES = [
-    'matplotlib',
-    'numpy',
-    'scipy',
-]
-
-TESTS_REQUIRES = [
-    'pytest>=3.6',
-    'pytest-cov'
-]
-
-EXTRAS_REQUIRES = {
-    'doc': [
-        'pandas',
-        'sphinx>=1.2',
-        'sphinx_rtd_theme'
-    ],
-    'tests': TESTS_REQUIRES
-}
-
-EXTRAS_REQUIRES['all'] = list(
-    set([v for deps in EXTRAS_REQUIRES.values() for v in deps])
-)
-
 PACKAGE_DATA = {
     'peakdet.tests': [
         'data/*'
     ]
 }
-
-CLASSIFIERS = [
-    'Development Status :: 3 - Alpha',
-    'Intended Audience :: Science/Research',
-    'License :: OSI Approved :: Apache Software License',
-    'Programming Language :: Python :: 3.6',
-]
